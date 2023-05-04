df33d5e0bbda73a6580cc242c9d4c1b72ac8ab83
Dennis Lönard <dennis.loenard97@gmx.de>
2023-05-04 10:13:19 +0200

    finished initial project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # minigrep
 
-Small lightweight grep clone, based on Chapter 12 of [The Rust Programming Language](https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html).
+Small grep clone, based on Chapter 12 of [The Rust Programming Language](https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html).
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use std::{env, fs};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    file_name: String,
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.file_name);
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_name = args[2].clone();
-    Config { query, file_name }
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
 }
